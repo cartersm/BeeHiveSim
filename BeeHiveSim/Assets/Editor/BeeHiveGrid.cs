4c83d2276f0c889b7482adfca6ea6f542db3aa59
cartersm <cartersm@rose-hulman.edu>
2016-05-05 19:54:10 -0400

    Generate Cells

--- BeeHiveSim/Assets/Editor/BeeHiveGrid.cs
+++ BeeHiveSim/Assets/Editor/BeeHiveGrid.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
 
 namespace Assets.Editor
 {
     public class BeeHiveGrid
     {
+        private int _x;
+        private int _y;
+        private int _z;
         // x, y, z
         public Cell[,,] Cells { get; private set; }
 
-        public BeeHiveGrid()
+        public BeeHiveGrid(int x = 20, int y = 20, int z = 20)
         {
             this.Cells = new Cell[20, 20, 20];
+            this._x = x;
+            this._y = y;
+            this._z = z;
+            for (var i = 0; i < x; i++)
+            {
+                for (var j = 0; j < y; j++)
+                {
+                    for (var k = 0; k < z; k++)
+                    {
+                        this.Cells[i, j, k] = new Cell(i, j, k);
+                    }
+                }
+            }
         }
 
         public Cell[,] GetAdjacentCells(Point3D location)
@@ -26,7 +44,7 @@ namespace Assets.Editor
             {
                 return Cells[location.x, location.y, location.z];
             }
-            catch (IndexOutOfRangeException e)
+            catch (IndexOutOfRangeException)
             {
                 return null;
             }
