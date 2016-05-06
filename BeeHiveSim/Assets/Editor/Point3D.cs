4756c6ebe61eb39ea2b729eb9043f9cf46a7b6f6
cartersm <cartersm@rose-hulman.edu>
2016-05-05 22:03:00 -0400

    VisualStudio stupidity

--- BeeHiveSim/Assets/Editor/Point3D.cs
+++ BeeHiveSim/Assets/Editor/Point3D.cs
@@ -2,15 +2,16 @@
 {
     public class Point3D
     {
+
+        public int x { get; set; }
+        public int y { get; set; }
+        public int z { get; set; }
+
         public Point3D(int x = 0, int y = 0, int z = 0)
         {
             this.x = x;
             this.y = y;
             this.z = z;
         }
-
-        public int x { get; set; }
-        public int y { get; set; }
-        public int z { get; set; }
     }
 }
