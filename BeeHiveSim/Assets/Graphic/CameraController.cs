fbaba7ca92f0dd7900232108ebf669bcbb978075
zxcsvd <pengb@rose-hulman.edu>
2016-05-05 21:22:36 -0400

    Made camera solid and decent

--- BeeHiveSim/Assets/Graphic/CameraController.cs
+++ BeeHiveSim/Assets/Graphic/CameraController.cs
@@ -3,7 +3,18 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    float speed = 1.5f;
+    float speed = 2.5f;
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 500.0f;
+    public float sensY = 500.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
     // Use this for initialization
     void Start()
     {
@@ -13,21 +24,36 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(new Vector3(speed*Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-speed*Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector3(0, -speed*Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector3(0, speed*Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        if (Input.GetMouseButton(0))
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
     }
 }
