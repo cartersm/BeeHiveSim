44db39628d2e5a09eebb41a8ecfe280b1c3fb27b
xxshadowfang <xxshadowfang@gmail.com>
2016-05-05 20:11:48 -0400

    some parse

---
+++ BeeHiveSim/Assets/Editor/ConfigParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Editor
+{
+    class ConfigParser
+    {
+        combo configurationsCombo [];
+    }
+
+    class combo
+    {
+        private LocalConfiguration config { get; set; }
+        private double prob { get; set; }
+
+
+    }
+}
