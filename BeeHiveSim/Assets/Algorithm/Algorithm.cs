369f9c6eed1b73dfa0f05be9eee70d886ddddc94
cartersm <sean47933@gmail.com>
2016-05-12 15:32:58 -0400

    Fix algorithm for grid method changes

--- BeeHiveSim/Assets/Algorithm/Algorithm.cs
+++ BeeHiveSim/Assets/Algorithm/Algorithm.cs
@@ -38,13 +38,20 @@ namespace Assets.Algorithm
         public void Start()
         {
             // place one brick at a predefined site
-            this.Grid.Cells[10, 10, 10].BrickType = 1;
-            this.Grid.Cells[10, 10, 10].IsOccupied = true;
+            this.Grid.OccupyCell(10, 10, 10);
+            this.Grid.SetBrickType(10, 10, 10, 1);
             var lookupTable = ConfigParser.Parse(this._filename);
 
             for (var k = 0; k < this._numBees; k++)
             {
                 var p = GetUnoccupiedPoint();
+                if (p.Z < 19)
+                {
+                    var p2 = new Point3D(p.X, p.Y, p.Z + 1);
+                    this.Grid.OccupyCell(p2);
+                    this.Grid.SetBrickType(p2, 1);
+                }
+
                 this.Grid.Cells[p.X, p.Y, p.Z].IsOccupied = true;
                 this._bees.Add(new Bee(k, new Point3D(p.X, p.Y, p.Z), lookupTable));
             }
@@ -59,7 +66,7 @@ namespace Assets.Algorithm
             {
                 var bee = this._bees[k];
                 // Sense local configuration
-                var cells = this.Grid.GetAdjacentCells(bee.Location);
+                var cells = this.Grid.GetAdjacentCells(bee.Location, true);
                 var config = new LocalConfiguration(cells);
                 var brickToPlace = bee.SenseEnvironment(config);
                 //Debug.Log(config);
