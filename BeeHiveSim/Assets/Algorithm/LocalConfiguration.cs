73e1576c28622ad9b5daeaf2b92fb8fb5358bd7c
cartersm <sean47933@gmail.com>
2016-05-12 15:32:33 -0400

    Fix LocalConfiguration equality
    array.equals just uses the base object.equals. Manually constructed a
    function to actually test equality.

--- BeeHiveSim/Assets/Algorithm/LocalConfiguration.cs
+++ BeeHiveSim/Assets/Algorithm/LocalConfiguration.cs
@@ -1,4 +1,7 @@
-﻿namespace Assets.Algorithm
+﻿using System.Text;
+using UnityEngine;
+
+namespace Assets.Algorithm
 {
     public class LocalConfiguration
     {
@@ -17,8 +20,39 @@
                 return false;
             }
 
-            var other = (LocalConfiguration) obj;
-            return this.Config.Equals(other.Config);
+            var other = (LocalConfiguration)obj;
+            return ConfigsEqual(this.Config, other.Config);
+        }
+
+        private static bool ConfigsEqual(Cell[,] config, Cell[,] config2)
+        {
+            //return MultiDArrayToString(config).Equals(MultiDArrayToString(config2));
+            if (config.Rank != config2.Rank || config.Length != config2.Rank) return false;
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 7; j++)
+                {
+                    if (config[i, j] != null && !config[i, j].Equals(config2[i, j])) return false;
+                }
+            }
+            return true;
+        }
+
+        private static string MultiDArrayToString(Cell[,] cells)
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < 3; i++)
+            {
+                for (var j = 0; j < 7; j++)
+                {
+                    var c = cells[i, j];
+                    if (c == null) continue;
+                    sb.Append(c.BrickType);
+                    sb.Append(",");
+                }
+                sb.Append("\n");
+            }
+            return sb.ToString();
         }
 
         // override object.GetHashCode
@@ -31,12 +65,28 @@
                 {
                     if (this.Config[i, j] != null)
                     {
-                        
-                        hash += ((i*j)^(this.Config[i, j].BrickType +3) - i*j);
+                        hash += ((i * j) ^ (this.Config[i, j].BrickType + 3) - i * j);
                     }
                 }
             }
             return hash;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Local Config:");
+            for (var i = 0; i < 3; i++)
+            {
+                sb.Append("\n  ");
+                for (var j = 0; j < 7; j++)
+                {
+                    if (this.Config[i, j] == null) continue;
+                    sb.Append(this.Config[i, j].BrickType);
+                    sb.Append(",");
+                }
+            }
+            return sb.ToString();
         }
     }
 }
