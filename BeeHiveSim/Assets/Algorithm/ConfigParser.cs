7f52b4b8d7e8d77109f9aa425589191a237e505d
cartersm <sean47933@gmail.com>
2016-05-13 11:34:25 -0400

    Update ConfigParser for rotations
    Rotationally equal configs are now included. I believe this was
    Bonabeau's intent with the given configs.

--- BeeHiveSim/Assets/Algorithm/ConfigParser.cs
+++ BeeHiveSim/Assets/Algorithm/ConfigParser.cs
@@ -27,25 +27,29 @@ namespace Assets.Algorithm
                     var mid = fields[1].Split(',');
                     var bot = fields[2].Split(',');
                     var brick = new BrickPlacement(int.Parse(fields[3]), double.Parse(fields[4]));
-                    var cells = new Cell[3, 7];
-                    for (var i = 0; i < 6; i++)
+                    for (var j = 0; j < 6; j++)
                     {
-                        cells[0, i] = new Cell(int.Parse(bot[i]));
-                        cells[1, i] = new Cell(int.Parse(mid[i]));
-                        cells[2, i] = new Cell(int.Parse(top[i]));
-                    }
-                    cells[0, 6] = new Cell(int.Parse(bot[6]));
-                    cells[1, 6] = null;
-                    cells[2, 6] = new Cell(int.Parse(top[6]));
+                        var cells = new Cell[3, 7];
+                        for (var i = 0; i < 6; i++)
+                        {
+                            var idx = (i + j) % 6;
+                            cells[0, i] = new Cell(int.Parse(bot[idx]));
+                            cells[1, i] = new Cell(int.Parse(mid[idx]));
+                            cells[2, i] = new Cell(int.Parse(top[idx]));
+                        }
+                        cells[0, 6] = new Cell(int.Parse(bot[6]));
+                        cells[1, 6] = null;
+                        cells[2, 6] = new Cell(int.Parse(top[6]));
 
-                    var config = new LocalConfiguration(cells);
-                    try
-                    {
-                        dict.Add(config, brick);
-                    }
-                    catch (ArgumentException)
-                    {
-                        Debug.LogWarning("Found Duplicate Config");
+                        var config = new LocalConfiguration(cells);
+                        try
+                        {
+                            dict.Add(config, brick);
+                        }
+                        catch (ArgumentException)
+                        {
+                            Debug.LogWarning("Found Duplicate Config");
+                        }
                     }
                 }
             }
