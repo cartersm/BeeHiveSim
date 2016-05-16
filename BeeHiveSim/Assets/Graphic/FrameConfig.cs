786418e0b29551b3f06fdb2d9298365579b0f002
zxcsvd <pengb@rose-hulman.edu>
2016-05-16 15:58:42 -0400

    New grid

---
+++ BeeHiveSim/Assets/Graphic/FrameConfig.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameConfig : MonoBehaviour
+{
+    public Transform start = new RectTransform();
+    public Transform end = new RectTransform();
+
+    void Start () {
+        start.position = new Vector3(0, 0, 0);
+        end.position = new Vector3(2, 2, 2);
+        Gizmos.color = Color.black;
+        Gizmos.DrawLine(start.position, end.position);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
